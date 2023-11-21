869b3dce123d1ae1dad8805bd0a80d4ef4bd8ae5
thijn1234 <48473867+thijn1234@users.noreply.github.com>
2023-11-21 10:15:40 +0100

    LT1OV-19 Vertrek en aankomst locatie kan worden geselecteerd

--- src/main/java/com/example/ovapp/HelloApplication.java
+++ src/main/java/com/example/ovapp/HelloApplication.java
@@ -1,22 +1,67 @@
 package com.example.ovapp;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.time.LocalTime;
 
+/**
+ * JavaFX OV application with automatic time refresh in the title.
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * Entry point of the application.
+     *
+     * @param primaryStage The primary stage for the application.
+     * @throws IOException If an error occurs while loading the FXML file.
+     */
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        Scene scene = new Scene(fxmlLoader.load(), 900, 800);
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(1), event -> {
+                    primaryStage.setTitle("OV Application - " + getCurrentTime());
+                })
+        );
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+
+        timeline.play();
+
+
+        //Magnifier magnifier = new Magnifier(scene);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+
     }
 
+    /**
+     * Get the current time as a formatted string.
+     *
+     * @return A string representing the current time.
+     */
+    private String getCurrentTime() {
+        LocalTime currentTime = LocalTime.now();
+        return "Current Time: " + currentTime.format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+    /**
+     * Main method to launch the application.
+     *
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/com/example/ovapp/HelloController.java
+++ src/main/java/com/example/ovapp/HelloController.java
@@ -1,14 +1,84 @@
 package com.example.ovapp;
 
+import javafx.scene.control.*;
+import javafx.util.StringConverter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private String Departure;
+    private String Arrival;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> choiceBoxArrival;
+    @FXML
+    private Label routeOutText;
+    @FXML
+    private ChoiceBox<String> choiceBoxDeparture;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Spinner<Double> timePicker;
+
+    @FXML
+    protected void SearchRoute() {
+        Departure = choiceBoxDeparture.getValue();
+        Arrival = choiceBoxArrival.getValue();
+        LocalDate selectedDate = datePicker.getValue();
+        Double selectedTime = timePicker.getValue();
+
+        try{
+            routeOutText.setText(String.format("Dit is de route van %s naar %s op %s om %.2f uur",
+                    Departure, Arrival, selectedDate.toString(), selectedTime));
+        } catch(NullPointerException e){
+            routeOutText.setText(" Vul A.U.B. alle velden in.");
+        }
+    }
+
+    @FXML
+    protected void initialize() {
+        initializeChoiceBox();
+        initializeTimePicker();
+    }
+
+    private void initializeChoiceBox() {
+        ObservableList<String> stations = FXCollections.observableArrayList("Amsterdam", "Amersfoort", "Enschede");
+
+        choiceBoxDeparture.setItems(stations);
+        choiceBoxArrival.setItems(stations);
+
+        // Set a default selection (optional)
+        choiceBoxDeparture.setValue(stations.get(0));
+        choiceBoxArrival.setValue(stations.get(0));
+
+    }
+    private void initializeTimePicker() {
+        SpinnerValueFactory<Double> valueFactory =
+                new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 23.55, 12.0, 0.05);
+
+        valueFactory.setConverter(new StringConverter<>() {
+            private final DecimalFormat decimalFormat = new DecimalFormat("00.00");
+
+            @Override
+            public String toString(Double value) {
+                return decimalFormat.format(value);
+            }
+
+            @Override
+            public Double fromString(String text) {
+                return Double.parseDouble(text);
+            }
+        });
+
+        timePicker.setValueFactory(valueFactory);
+
+        timePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+        });
     }
 }
